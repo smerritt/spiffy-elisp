1628c6c8a472055665337fb7e358dcf94f9b88f5
Sam Merritt <spam@andcheese.org>
2009-01-31 13:52:45 -0800

    GPL this stuff in case anyone ever finds it and wants to use it (ha!)

--- spiffy-expectations.el
+++ spiffy-expectations.el
@@ -1,3 +1,14 @@
+;; Tests for spiffy.el: a bunch of stuff I use in my Ruby+Merb coding.
+;; Probably not useful to anyone but me, but hey, give it a try.
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; version 2 of the License.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
 (require 'el-expectations)
 (require 'spiffy)
 (require 'fancy-pants-function-replacement)
--- spiffy.el
+++ spiffy.el
@@ -1,3 +1,14 @@
+;; spiffy.el: a bunch of stuff I use in my Ruby+Merb coding.
+;; Probably not useful to anyone but me, but hey, give it a try.
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; version 2 of the License.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
 (provide 'spiffy)
 
 (defun spiffy-cwd ()
